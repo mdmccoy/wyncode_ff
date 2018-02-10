8c16c0ac42921a376f749f44d3c7546bf3ec028f
mdmccoy <mdmccoy@gmail.com>
2018-02-10 13:49:56 -0500

    Finished up review model/tests.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
-  validates :description, :author, :picture_id, presence: true
+  validates :description, :picture_id, presence: true
+  validates :author, presence: true, length: { maximum: 255 }
   validates_inclusion_of :rating, in: 1..5
 
   belongs_to :picture
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -11,4 +11,14 @@ class ReviewTest < ActiveSupport::TestCase
   test 'should be valid' do
     assert @review.valid?
   end
+
+  test 'rating should be between 1-5' do
+    @review.rating = 6
+    assert_not @review.valid?
+  end
+
+  test 'author should not be too long' do
+    @review.author = 'a' * 256
+    assert_not @review.valid?
+  end
 end
