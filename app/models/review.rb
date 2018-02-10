acbd9aaa324be8146cbe838fb62b6e3c26b4777d
Asger <sirjazzper@gmail.com>
2018-02-10 11:56:06 -0500

    restricting rating on reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
-  validates :description, :author, :rating, :picture_id, presence: true
+  validates :description, :author, :picture_id, presence: true
+  validates_inclusion_of :rating, :in => 1..5
   belongs_to :picture
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 Picture.create([{url:'http://www.dailyherald.com/storyimage/DA/20110724/entlife/707249979/AR/0/AR-707249979.jpg&updated=201107241250&MaxW=800&maxH=800&noborder',
                  caption: 'Sweet pic!',
                  rating: 4},
-                 url:'https://cdn.vox-cdn.com/thumbor/LBx-fZEtigqICpLxtnJVdMe2HqE=/0x0:1496x1112/920x613/filters:focal(445x396:683x634):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/50524581/Screen_20Shot_202016-08-25_20at_201.00.22_20PM.0.png',
-                 caption: 'Super sweet pic!'
+                 {url:'https://cdn.vox-cdn.com/thumbor/LBx-fZEtigqICpLxtnJVdMe2HqE=/0x0:1496x1112/920x613/filters:focal(445x396:683x634):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/50524581/Screen_20Shot_202016-08-25_20at_201.00.22_20PM.0.png',
+                 caption: 'Super sweet pic!',
                  rating: 3}
                 ])
