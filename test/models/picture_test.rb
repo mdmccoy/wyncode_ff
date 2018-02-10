6e848083e023969006dececa9771fb6160c0dcf9
mdmccoy <mdmccoy@gmail.com>
2018-02-10 11:08:54 -0500

    Fixed fixtures

--- test/models/picture_test.rb
+++ test/models/picture_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class PictureTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @picture = Picture.new(url: 'www.something.com',
+                           caption: 'present',
+                           location: 'something',
+                           rating: 5)
+  end
+
+  test 'should be valid' do
+    assert @picture.valid?
+  end
 end
