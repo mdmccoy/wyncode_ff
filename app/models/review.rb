5150172064f83437a5f262aa0dfba08f51d6a716
mdmccoy <mdmccoy@gmail.com>
2018-02-11 11:14:06 -0500

    Changed ratings in form partials to dropdowns.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
+  RATING_VALS = [1, 2, 3, 4, 5].freeze
+
   validates :description, :picture_id, presence: true
   validates :author, presence: true, length: { maximum: 255 }
   validates_inclusion_of :rating, in: 1..5
