fa844f65e6ab6b80da50c0ffe44e7cd0c87801c3
mdmccoy <mdmccoy@gmail.com>
2018-02-11 21:23:50 -0500

    Updated about page and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :pictures
   resources :reviews
-  resources :static_pages
+  get '/about' => 'static_pages#index'
 
   root 'pictures#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
