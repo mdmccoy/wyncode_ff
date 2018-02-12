792f86587d056b736875d7d40d1fb54c8c1a9f6d
mdmccoy <mdmccoy@gmail.com>
2018-02-12 13:20:55 -0500

    Working on top5

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :reviews
   get '/about' => 'static_pages#index'
 
+  get '/top5' => 'pictures#top5'
+
   root 'pictures#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
